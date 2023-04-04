acd361fb9348bf6aa05f42b4d7e33b9cffba5009
Giovannyf99 <giovannyf1999@gmail.com>
2023-04-04 14:01:48 -0400

    inital commit

---
+++ js/quiz.js
@@ -0,0 +1,16 @@
+let answerContainer = document.getElementById('answer-boxs')
+let onClick = document.querySelector('onclick')
+
+let firstButton = doucument.getElementById('one')
+let secondButton = doucument.getElementById('two')
+let thirdButton = doucument.getElementById('three')
+let fourthButton = doucument.getElementById('four')
+
+let answers = []
+
+
+function quiz (){
+    answerContainer.addEventListener('click', function() {
+
+    })
+}
