dff6af5c0ca916c6679eb3facaec70de98bfee5b
Giovannyf99 <giovannyf1999@gmail.com>
2023-04-05 09:48:10 -0400

    commit

--- js/quiz.js
+++ js/quiz.js
@@ -1,4 +1,26 @@
-document.getElementById("myButton").onclick = function(){
-    let myName = document.getElementById("myText").value;
-    console.log("hello", myName)
+let questionsArrayPlaceholder = document.body.appendChild(document.createElement("div"));
+
+let questionsArray = [];
+
+let btn1 = document.getElementById("btn1")
+
+let climate = document.getElementById('climate').value
+let continent = document.getElementById('continent').value;
+let geography = document.getElementById('geography').value;
+
+questionsArray.push(climate.value, continent.value, geography.value)
+
+function getUserInput() {
+    btn1.addEventListener("click", function () {
+        climate.value
+        document.getElementById('questions').innerHTML = climate + " " + continent + " " + geography;
+    })
 }
+
+function data() {
+    btn1.addEventListener("click", function () {
+        questions.innerHTML = climate + " " + continent + " " + geography;
+        console.log(questions)
+    })
+}
+
