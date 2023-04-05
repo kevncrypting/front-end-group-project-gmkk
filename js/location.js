c838aef8170e283dcf13a1f44023db20f5e8fcff
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-04-05 10:38:06 -0400

    Adds temp link again after G deleted it

--- js/location.js
+++ js/location.js
@@ -1,3 +1,2 @@
 // this is how you import the API token into the .js file - from here on, ONLY use the API key as a variable! DO NOT copy and paste API key directly
 import API_TOKEN from "./js/config.js"
-
