f28cd7d7a0c27778f65fbaedd5ffeb4c9a1dbbfe
kblack-git <keithblack@gmail.com>
2023-04-05 11:45:33 -0400

    added config.js, exported API key, imported

--- js/config-template.js
+++
@@ -1,3 +0,0 @@
-const API_TOKEN = ''; // this is where you copy and paste your API token
-
-export default API_TOKEN;
--- js/location.js
+++ js/location.js
@@ -1,3 +1,10 @@
 // this is how you import the API token into the .js file - from here on, ONLY use the API key as a variable! DO NOT copy and paste API key directly
-import API_TOKEN from "./js/config.js"
+import API_TOKEN from "./config"
+
+const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+fetch(`https://api.content.tripadvisor.com/api/v1/location/search?key=${API_TOKEN}&searchQuery=%27london%27&language=en`, options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
 
