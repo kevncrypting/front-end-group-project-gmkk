70db19d2c43f377481617b6f1c73e205cd0c7407
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-04-06 10:24:21 -0400

    Adjusts getCityData function to return just the placeID for the input location

--- js/location.js
+++ js/location.js
@@ -2,11 +2,12 @@ import API_TOKEN from "./config.js"
 
 // const options = { method: 'GET', headers: { accept: 'application/json' } };
 
-let form = document.querySelector('form');
-let inputCity = document.getElementById('input-city');
+let form = document.querySelector('form'); // targets form on page
+let inputCity = document.getElementById('input-city'); // targets input box on page
 let cityName = document.getElementById('city-name');
 
 let cityInput = ``;
+let cityPlaceID = ``;
 
 form.addEventListener('submit', (event) => { // listens for submission of form
     event.preventDefault(); // prevents page refresh
@@ -21,8 +22,17 @@ function getCityData(city, apiKey) { // found this function in a tutorial on the
     .then(data => data.json())
     .then(results => {
         const cityData = results.results.length ? results.results[0] : null;
+        cityPlaceID = cityData.place_id
+        console.log(cityPlaceID);
         return cityData;
     });
 }
 
+let getHotels = placeID => {
+
+}
+
+// https://api.geoapify.com/v2/places?categories=${category}&filter=place:${cityId}&limit=${limit}&apiKey=${apiKey}&offset=${offset}
+
 // Goal is to have user able to input a location and display 5 hotels, restaurants, and attractions.
+
